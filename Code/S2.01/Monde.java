6c109955319c454774248a9b9ca2724cda4cb0e9
Théo Bernaville <92569493+Legeones@users.noreply.github.com>
2022-05-02 10:27:19 +0200

    Update Monde.java

--- Code/S2.01/Monde.java
+++ Code/S2.01/Monde.java
@@ -10,4 +10,12 @@ public class Monde {
 	public void addSecteur(Secteur sec) {
 		this.lesSecteurs.add(sec);
 	}
+	
+	public Monde createWorld()
+	{
+		for(int i=0;i<=100;i++) {
+			this.addSecteur(new Secteur());
+		}
+		return this;
+	}
 }
