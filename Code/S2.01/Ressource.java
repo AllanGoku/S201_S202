f164806a11e7f37504b167859caa4c3abf251f27
BastienH <92568765+BastienHot@users.noreply.github.com>
2022-05-02 10:46:49 +0200

    Update erreur

--- Code/S2.01/Ressource.java
+++ Code/S2.01/Ressource.java
@@ -2,7 +2,7 @@ public abstract class Ressource{
 	private int type;
 	private int capacite;
 	public abstract int getNbrMinerai();
-	public abstract void setNbrMinerai(String n);
+	public abstract void setNbrMinerai(int n);
 	public abstract int getCoord();
 	public abstract void setCoord(int n);
 }
