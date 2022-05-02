01f8da7983f7aec651b51420f5b29736f2404eb1
BastienH <92568765+BastienHot@users.noreply.github.com>
2022-05-02 10:29:00 +0200

    Ajout de méthodes

--- Code/S2.01/Ressource.java
+++ Code/S2.01/Ressource.java
@@ -3,4 +3,6 @@ public abstract class Ressource{
 	private int capacite;
 	public abstract int getNbrMinerai();
 	public abstract void setNbrMinerai(String n);
+	public abstract int getCoord();
+	public abstract void setCoord(int n);
 }
