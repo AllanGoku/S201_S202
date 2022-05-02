ab68898113a10e88cca17a362c2e9a07bf4c4641
BastienH <92568765+BastienHot@users.noreply.github.com>
2022-05-02 10:17:37 +0200

    Update nom de méthode

--- Code/S2.01/Ressource.java
+++ Code/S2.01/Ressource.java
@@ -2,5 +2,5 @@ public abstract class Ressource{
 	private int type;
 	private int capacite;
 	public abstract int getNbrMinerai();
-	public abstract void setMinerai(String m);
+	public abstract void setNbrMinerai(String n);
 }
