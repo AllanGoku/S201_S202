49c1aa1973528069aae0696e15aba9946ffaf65a
AllanGoku <102809467+AllanGoku@users.noreply.github.com>
2022-05-06 09:10:17 +0200

    Ajout des coordonnées dans le constructeur

--- Code/S2.01/Mine.java
+++ Code/S2.01/Mine.java
@@ -3,15 +3,15 @@ public class Mine extends Ressource {
 	
 	private int capacite;
 	
-	public Mine()
+	public Mine(int x, int y)
 	{
 		setNumero(0);
 		setTypeMinerai("inconnu");
 		this.capacite=0;
-		this.setSonSecteur(new Secteur());
+		this.setSonSecteur(new Secteur(x,y));
 	}
 	
-	public Mine(int n, String t, int c, int x, Secteur sec)
+	public Mine(int n, String t, int c, Secteur sec)
 	{
 		setNumero(n);
 		setTypeMinerai(t);
