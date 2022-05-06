591369cf68bb2bc83defa7f6dc68a2ac86f1f579
AllanGoku <102809467+AllanGoku@users.noreply.github.com>
2022-05-06 09:09:37 +0200

    Ajout des coordonnées dans le constructeur

--- Code/S2.01/Entrepot.java
+++ Code/S2.01/Entrepot.java
@@ -3,15 +3,15 @@ public class Entrepot extends Ressource {
 	
 	private int capacite;
 	
-	public Entrepot()
+	public Entrepot(int x, int y)
 	{
 		setNumero(0);
 		setTypeMinerai("inconnu");
 		this.capacite=0;
-		this.setSonSecteur(new Secteur());
+		this.setSonSecteur(new Secteur(x,y));
 	}
 	
-	public Entrepot(int n, String t, int c, int x, Secteur sec)
+	public Entrepot(int n, String t, int c,Secteur sec)
 	{
 		setNumero(n);
 		setTypeMinerai(t);
