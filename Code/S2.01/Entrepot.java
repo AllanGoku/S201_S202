bac0e66374305983260c5bf7e15d09a313be12ba
AllanGoku <102809467+AllanGoku@users.noreply.github.com>
2022-05-06 08:50:30 +0200

    Mise à jour Entrepot
    Suppression des attributs numéro, type, row et secteur car c'est attribut son définie dans Ressources.

--- Code/S2.01/Entrepot.java
+++ Code/S2.01/Entrepot.java
@@ -1,50 +1,30 @@
 
 public class Entrepot extends Ressource {
 	
-	private int numero;
-	private String type;
 	private int capacite;
-	private int row;
-	private Secteur secteur;
 	
 	public Entrepot()
 	{
-		this.numero = 0;
-		this.type="inconnu";
+		setNumero(0);
+		setTypeMinerai("inconnu");
 		this.capacite=0;
-		this.row=0;
-		this.secteur=new Secteur();
+		this.setSonSecteur(new Secteur());
 	}
 	
 	public Entrepot(int n, String t, int c, int x, Secteur sec)
 	{
-		this.numero = n;
-		this.type=t;
+		setNumero(n);
+		setTypeMinerai(t);
 		this.capacite=c;
-		this.row=x;
-		this.secteur= sec;
+		this.setSonSecteur(sec);
 	}
 	
-	@Override
-	public int getCoord()
-	{
-		return this.row;
-	}
-	
-	@Override
-	public void setCoord(int n)
-	{
-		this.row=n;
-	}
-	
-	@Override
+
 	public int getNbrMinerai() {
 		return this.capacite;
 	}
 
-	@Override
 	public void setNbrMinerai(int n) {
 		this.capacite = n;
 	}
 }
-
