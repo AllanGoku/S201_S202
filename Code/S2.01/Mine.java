8627aaa9b802d547e94c9c43a2fb36b98dea9040
AllanGoku <102809467+AllanGoku@users.noreply.github.com>
2022-05-09 10:46:36 +0200

    Suppresion de la variable capacite et ajout "capaciteStockageMax" dans constructeur
    Suppresion de la varaible "capacite" car celle-ci est maintenant présentes dans la classe "Ressources" ainsi que la modification des constructeurs avec l'ajout de la capacité maximum possible

--- Code/S2.01/Mine.java
+++ Code/S2.01/Mine.java
@@ -1,33 +1,32 @@
-package application;
 
 public class Mine extends Ressource {
-	
-	private int capacite;
 	
 	public Mine(int x, int y)
 	{
 		setNumero(0);
 		setTypeMinerai("inconnu");
-		this.capacite=0;
+		this.setCapacite(0);
+		this.setCapaciteStockageMax(0);
 		this.setSonSecteur(new Secteur(x,y));
 	}
 	
-	public Mine(int n, String t, int c, Secteur sec)
+	public Mine(int n, String t, int c,Secteur sec)
 	{
 		setNumero(n);
 		setTypeMinerai(t);
-		this.capacite=c;
+		this.setCapaciteStockageMax(c);
+		this.setCapacite(0);
 		this.setSonSecteur(sec);
 	}
 
 
 	public int getNbrMinerai() {
-		return capacite;
+		return getCapacite();
 	}
 
 	
 	public void setNbrMinerai(int n) {
-		this.capacite=n;
+		this.setCapacite(n);
 	}
 	
 
